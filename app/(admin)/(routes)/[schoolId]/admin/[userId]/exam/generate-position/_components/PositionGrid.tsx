71460c6cadfe367cbfc8616bf0548e0c8067289a
Godwin Adu <gyamfiadu01@gmail.com>
2025-02-09 23:18:53 +0000

    make changes

--- app/(admin)/(routes)/[schoolId]/admin/[userId]/exam/generate-position/_components/PositionGrid.tsx
+++ app/(admin)/(routes)/[schoolId]/admin/[userId]/exam/generate-position/_components/PositionGrid.tsx
@@ -1,32 +1,23 @@
 "use client"
-import ClassSelection from '@/components/attendance/ClassSelection'
+
 import { Button } from '@/components/ui/button'
 import { Loader2 } from 'lucide-react'
 import React, { useEffect, useState } from 'react';
-import { AgGridReact } from 'ag-grid-react'; // React Data Grid Component
-import "ag-grid-community/styles/ag-grid.css"; // Mandatory CSS required by the grid
-import "ag-grid-community/styles/ag-theme-quartz.css"; // Optional Theme applied to the grid
-import { CellAction } from './cell-action'
-import { fetchStudentsList } from '@/lib/actions/student.actions'
 import { fetchAllMarks, generatePosition } from '@/lib/actions/mark-entries.actions';
 import moment from 'moment';
 import { getOrdinalSuffix } from '@/lib/utils';
 import { toast } from '@/hooks/use-toast';
 import { useRouter } from 'next/navigation';
+import ClassSelection from '@/components/commons/ClassSelection';
 
 const PositionGrid = ({ classes }: { classes: IClass[] }) => {
     const [selectedClass, setSelectedClass] = useState("");
     const [isLoading, setIsLoading] = useState<boolean>(false)
     const [isLoadingGenerate, setIsLoadingGenerate] = useState<boolean>(false)
+    const [rowData, setRowData] = useState([]);
 
     const router = useRouter()
 
-    // Row Data: The data to be displayed.
-    const [rowData, setRowData] = useState([]);
-    console.log(rowData,"row data")
-
-    // Column Definitions: Defines the columns to be displayed.
-    const [colDefs, setColDefs] = useState<{ field: string, width?: number, editable?: boolean }[]>([])
     useEffect(() => {
         const fetchPayment = async () => {
             try {
@@ -54,7 +45,7 @@ const PositionGrid = ({ classes }: { classes: IClass[] }) => {
             { field: "createdBy" },
             { field: "createdAt" },
         ];
-        setColDefs(newColumnDefs);
+
         fetchPayment()
     }, []);
 
@@ -98,12 +89,7 @@ const PositionGrid = ({ classes }: { classes: IClass[] }) => {
         }
     };
 
-    /**
-  * Pagination settings for the grid.
-  */
-    const pagination = true;
-    const paginationPageSize = 200;
-    const paginationPageSizeSelector = [200, 500, 1000];
+
     return (
         <>
             <div className="border py-1 px-4 flex gap-5 items-center my-1">
@@ -124,18 +110,7 @@ const PositionGrid = ({ classes }: { classes: IClass[] }) => {
             </div>
             <div className="py-4 mt-2 px-2">
 
-                <div
-                    className="ag-theme-quartz" // applying the grid theme
-                    style={{ height: 500, width: "100%" }} // the grid will fill the size of the parent container
-                >
-                    <AgGridReact
-                        rowData={rowData}
-                        columnDefs={colDefs}
-                        pagination={pagination}
-                        paginationPageSize={paginationPageSize}
-                        paginationPageSizeSelector={paginationPageSizeSelector}
-                    />
-                </div>
+               
             </div>
 
         </>
